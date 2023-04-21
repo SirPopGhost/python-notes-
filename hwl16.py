c998d2dddf02ed7c1cfcbe155e16dc6e1063c005
OwnerSri <OwnerSri@MacPersonalRD2020.local>
2023-04-20 17:11:56 -0700

    Feat: Homework 16 Opening and Reading functions

--- hwl16.py
+++ hwl16.py
@@ -1,13 +1,16 @@
 def data_from_file(data_file):
-    data_list = []
+    result_list = []
     file = open(data_file, 'r')
     read_data = file.read()
-    data_list.append(read_data)
-    return data_list
+    data_list = read_data.split('\n')
+    for integer in data_list:
+        integer_list = int(integer)
+        result_list.append(integer_list)
+    return result_list
 
 def main():
     input_file = "data.dat"
     read_data = data_from_file(input_file)
-    return read_data
+    print(read_data)
 
-print(main())
+main()
