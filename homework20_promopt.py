4a3c44638307aae789820607ead189853c856fde
Parth Shah <parth@Parths-MacBook-Pro.local>
2023-05-04 17:33:44 -0700

    feat: Added instructions for parsing project

---
+++ homework20_promopt.py
@@ -0,0 +1,22 @@
+"""
+
+From the given data we want to find what is average demograpohic of our students in the class.
+1. What is the average age of the student in the class?
+2. What percentage  of students are male and female?
+3. What is the highest gpa in the class and what is the lowest gpa?
+4. How many students are enrolled in the class?
+
+Write the answers of above questions in a separate file.
+
+Notes:
+Make individual functions for each of the probelm when it makes sense.
+Follow the general structure of code:
+
+functions
+
+main
+ -- All the function call should happen here. Main should drive the flow of the program.
+
+main function call
+
+"""
