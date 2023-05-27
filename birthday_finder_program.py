4287125d187a44fc97d4cb26611a3a5c8bd9a997
Parth Shah <parth@Parths-MacBook-Pro.local>
2023-05-26 17:34:12 -0700

    feat: Adding specs for birthday application

---
+++ birthday_finder_program.py
@@ -0,0 +1,13 @@
+
+"""
+Sent an email and text message to the person wishing them happy birthday on their birthday.
+
+requirements:
+Have a database with people's phone number and birthdates
+on the day of thier birthday program will send them a text and email saying happy birthday.
+This program will be running everyday and checking 10 am everyday for any birthdays.
+
+Design decisions:
+Database -- need to secure and not open to public
+Code -- Need to utilize existing code from the previous exercise
+"""
