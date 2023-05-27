3d3fb1bd91b4ca1ff5b15006f5f7e79e49e22b0d
Parth Shah <parth@Parths-MacBook-Pro.local>
2023-05-26 17:47:30 -0700

    feat: Adding homework for the project

--- birthday_finder_program.py
+++ birthday_finder_program.py
@@ -10,4 +10,10 @@ This program will be running everyday and checking 10 am everyday for any birthd
 Design decisions:
 Database -- need to secure and not open to public
 Code -- Need to utilize existing code from the previous exercise
+
+Homework:
+Create an app.py for the application.
+In the app.py, create a function to read from the database file.
+Read the data from the database file,
+Using datetime module, see if today is anyone's birthday, by comparing it with data read from the database.
 """
